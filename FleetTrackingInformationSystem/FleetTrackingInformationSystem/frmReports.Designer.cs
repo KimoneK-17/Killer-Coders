d4efe1dbcf55fa463580132333c24e08c77a141e
MlondiSithole <steezyflacco@gmail.com>
2016-10-03 12:14:18 +0200

    Reports

--- FleetTrackingInformationSystem/FleetTrackingInformationSystem/frmReports.Designer.cs
+++ FleetTrackingInformationSystem/FleetTrackingInformationSystem/frmReports.Designer.cs
@@ -48,11 +48,35 @@
             this.mnuOptions = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuBack = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgvVehicleStatus = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
+            this.dataGridView6 = new System.Windows.Forms.DataGridView();
+            this.dataGridView7 = new System.Windows.Forms.DataGridView();
+            this.dataGridView8 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.VehicleBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.FleetTrackingDBDataSet)).BeginInit();
             this.tabReports.SuspendLayout();
+            this.tabVehicleStat.SuspendLayout();
+            this.tabServiceAppoint.SuspendLayout();
+            this.tabServiceRequire.SuspendLayout();
+            this.tabVehicleServiceCompleted.SuspendLayout();
+            this.tabDetailedServiceInfo.SuspendLayout();
+            this.tabPlannedTrip.SuspendLayout();
+            this.tabCompletedTrip.SuspendLayout();
+            this.tabTimesheet.SuspendLayout();
             this.pnlCustomer.SuspendLayout();
             this.mnuFleet.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVehicleStatus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView8)).BeginInit();
             this.SuspendLayout();
             // 
             // VehicleBindingSource
@@ -83,6 +107,7 @@
             // 
             // tabVehicleStat
             // 
+            this.tabVehicleStat.Controls.Add(this.dgvVehicleStatus);
             this.tabVehicleStat.Location = new System.Drawing.Point(4, 22);
             this.tabVehicleStat.Name = "tabVehicleStat";
             this.tabVehicleStat.Padding = new System.Windows.Forms.Padding(3);
@@ -93,6 +118,7 @@
             // 
             // tabServiceAppoint
             // 
+            this.tabServiceAppoint.Controls.Add(this.dataGridView2);
             this.tabServiceAppoint.Location = new System.Drawing.Point(4, 22);
             this.tabServiceAppoint.Name = "tabServiceAppoint";
             this.tabServiceAppoint.Padding = new System.Windows.Forms.Padding(3);
@@ -103,6 +129,7 @@
             // 
             // tabServiceRequire
             // 
+            this.tabServiceRequire.Controls.Add(this.dataGridView3);
             this.tabServiceRequire.Location = new System.Drawing.Point(4, 22);
             this.tabServiceRequire.Name = "tabServiceRequire";
             this.tabServiceRequire.Size = new System.Drawing.Size(1015, 446);
@@ -112,6 +139,7 @@
             // 
             // tabVehicleServiceCompleted
             // 
+            this.tabVehicleServiceCompleted.Controls.Add(this.dataGridView4);
             this.tabVehicleServiceCompleted.Location = new System.Drawing.Point(4, 22);
             this.tabVehicleServiceCompleted.Name = "tabVehicleServiceCompleted";
             this.tabVehicleServiceCompleted.Size = new System.Drawing.Size(1015, 446);
@@ -121,6 +149,7 @@
             // 
             // tabDetailedServiceInfo
             // 
+            this.tabDetailedServiceInfo.Controls.Add(this.dataGridView5);
             this.tabDetailedServiceInfo.Location = new System.Drawing.Point(4, 22);
             this.tabDetailedServiceInfo.Name = "tabDetailedServiceInfo";
             this.tabDetailedServiceInfo.Size = new System.Drawing.Size(1015, 446);
@@ -130,6 +159,7 @@
             // 
             // tabPlannedTrip
             // 
+            this.tabPlannedTrip.Controls.Add(this.dataGridView6);
             this.tabPlannedTrip.Location = new System.Drawing.Point(4, 22);
             this.tabPlannedTrip.Name = "tabPlannedTrip";
             this.tabPlannedTrip.Size = new System.Drawing.Size(1015, 446);
@@ -139,6 +169,7 @@
             // 
             // tabCompletedTrip
             // 
+            this.tabCompletedTrip.Controls.Add(this.dataGridView7);
             this.tabCompletedTrip.Location = new System.Drawing.Point(4, 22);
             this.tabCompletedTrip.Name = "tabCompletedTrip";
             this.tabCompletedTrip.Size = new System.Drawing.Size(1015, 446);
@@ -148,6 +179,7 @@
             // 
             // tabTimesheet
             // 
+            this.tabTimesheet.Controls.Add(this.dataGridView8);
             this.tabTimesheet.Location = new System.Drawing.Point(4, 22);
             this.tabTimesheet.Name = "tabTimesheet";
             this.tabTimesheet.Size = new System.Drawing.Size(1015, 446);
@@ -212,6 +244,71 @@
             this.mnuExit.Text = "Exit";
             this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
             // 
+            // dgvVehicleStatus
+            // 
+            this.dgvVehicleStatus.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVehicleStatus.Location = new System.Drawing.Point(0, 3);
+            this.dgvVehicleStatus.Name = "dgvVehicleStatus";
+            this.dgvVehicleStatus.Size = new System.Drawing.Size(1015, 479);
+            this.dgvVehicleStatus.TabIndex = 0;
+            this.dgvVehicleStatus.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(1019, 483);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(-4, 0);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(1023, 450);
+            this.dataGridView3.TabIndex = 0;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(-4, 0);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.Size = new System.Drawing.Size(1023, 533);
+            this.dataGridView4.TabIndex = 0;
+            // 
+            // dataGridView5
+            // 
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.Size = new System.Drawing.Size(1019, 493);
+            this.dataGridView5.TabIndex = 0;
+            // 
+            // dataGridView6
+            // 
+            this.dataGridView6.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView6.Location = new System.Drawing.Point(-4, 0);
+            this.dataGridView6.Name = "dataGridView6";
+            this.dataGridView6.Size = new System.Drawing.Size(1032, 483);
+            this.dataGridView6.TabIndex = 0;
+            // 
+            // dataGridView7
+            // 
+            this.dataGridView7.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView7.Location = new System.Drawing.Point(-4, 0);
+            this.dataGridView7.Name = "dataGridView7";
+            this.dataGridView7.Size = new System.Drawing.Size(1016, 517);
+            this.dataGridView7.TabIndex = 0;
+            // 
+            // dataGridView8
+            // 
+            this.dataGridView8.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView8.Location = new System.Drawing.Point(-4, 0);
+            this.dataGridView8.Name = "dataGridView8";
+            this.dataGridView8.Size = new System.Drawing.Size(1045, 496);
+            this.dataGridView8.TabIndex = 0;
+            // 
             // frmReports
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -228,10 +325,26 @@
             ((System.ComponentModel.ISupportInitialize)(this.VehicleBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.FleetTrackingDBDataSet)).EndInit();
             this.tabReports.ResumeLayout(false);
+            this.tabVehicleStat.ResumeLayout(false);
+            this.tabServiceAppoint.ResumeLayout(false);
+            this.tabServiceRequire.ResumeLayout(false);
+            this.tabVehicleServiceCompleted.ResumeLayout(false);
+            this.tabDetailedServiceInfo.ResumeLayout(false);
+            this.tabPlannedTrip.ResumeLayout(false);
+            this.tabCompletedTrip.ResumeLayout(false);
+            this.tabTimesheet.ResumeLayout(false);
             this.pnlCustomer.ResumeLayout(false);
             this.pnlCustomer.PerformLayout();
             this.mnuFleet.ResumeLayout(false);
             this.mnuFleet.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVehicleStatus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView8)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -257,5 +370,13 @@
         private System.Windows.Forms.ToolStripMenuItem mnuOptions;
         private System.Windows.Forms.ToolStripMenuItem mnuBack;
         private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.DataGridView dgvVehicleStatus;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.DataGridView dataGridView5;
+        private System.Windows.Forms.DataGridView dataGridView6;
+        private System.Windows.Forms.DataGridView dataGridView7;
+        private System.Windows.Forms.DataGridView dataGridView8;
     }
 }
