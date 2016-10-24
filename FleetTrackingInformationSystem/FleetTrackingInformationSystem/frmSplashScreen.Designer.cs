014bbc00ad6256d8c38ea925650734e234ea3300
nikster5555 <nikster5555>
2016-10-24 12:56:01 +0200

    Changed Splash Screen Design

--- FleetTrackingInformationSystem/FleetTrackingInformationSystem/frmSplashScreen.Designer.cs
+++ FleetTrackingInformationSystem/FleetTrackingInformationSystem/frmSplashScreen.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSplashScreen));
             this.picCargoFleet = new System.Windows.Forms.PictureBox();
+            this.lblStarting = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picCargoFleet)).BeginInit();
             this.SuspendLayout();
             // 
@@ -37,12 +38,22 @@
             // 
             this.picCargoFleet.BackColor = System.Drawing.Color.Transparent;
             this.picCargoFleet.Image = ((System.Drawing.Image)(resources.GetObject("picCargoFleet.Image")));
-            this.picCargoFleet.Location = new System.Drawing.Point(25, 552);
+            this.picCargoFleet.Location = new System.Drawing.Point(12, 12);
             this.picCargoFleet.Name = "picCargoFleet";
             this.picCargoFleet.Size = new System.Drawing.Size(247, 172);
             this.picCargoFleet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.picCargoFleet.TabIndex = 14;
             this.picCargoFleet.TabStop = false;
+            // 
+            // lblStarting
+            // 
+            this.lblStarting.AutoSize = true;
+            this.lblStarting.BackColor = System.Drawing.Color.Transparent;
+            this.lblStarting.Location = new System.Drawing.Point(597, 12);
+            this.lblStarting.Name = "lblStarting";
+            this.lblStarting.Size = new System.Drawing.Size(52, 13);
+            this.lblStarting.TabIndex = 15;
+            this.lblStarting.Text = "Starting...";
             // 
             // frmSplashScreen
             // 
@@ -50,8 +61,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(661, 785);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(661, 451);
+            this.Controls.Add(this.lblStarting);
             this.Controls.Add(this.picCargoFleet);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -63,11 +75,13 @@
             this.Text = "Fleet Tracking Information System";
             ((System.ComponentModel.ISupportInitialize)(this.picCargoFleet)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox picCargoFleet;
+        private System.Windows.Forms.Label lblStarting;
     }
 }
